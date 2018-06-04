995c42d3f3ecf26cb66bafaae608037cd9f2bbb3
Steve <sstpierre2@partners.org>
2018-06-04 13:33:43 -0400

    Change scrollBy to match use of ScrollView instead of ViewPagerAndroid

--- src/index.js
+++ src/index.js
@@ -462,11 +462,7 @@ export default class extends Component {
     if (state.dir === 'x') x = diff * state.width
     if (state.dir === 'y') y = diff * state.height
 
-    if (Platform.OS !== 'ios') {
-      this.scrollView && this.scrollView[animated ? 'setPage' : 'setPageWithoutAnimation'](diff)
-    } else {
-      this.scrollView && this.scrollView.scrollTo({ x, y, animated })
-    }
+    this.scrollView && this.scrollView.scrollTo({ x, y, animated })
 
     // update scroll state
     this.internals.isScrolling = true
